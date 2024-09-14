de0f21d13e39bd289a2d41c604258759f9f4f717
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-09-15 04:14:24 +0900

    feat(hold): Balance check to match plans prices

--- src/providers/hold.py
+++ src/providers/hold.py
@@ -5,8 +5,12 @@ from fastapi import APIRouter, HTTPException
 
 from src.config import config
 from src.interfaces.hold import HoldPostSubscribeBody, HoldAggregateData
+from src.interfaces.subscription import SubscriptionType
 
 router = APIRouter(tags=["Hold provider"])
+
+# TODO: update these placeholder prices
+ltai_hold_prices: dict[SubscriptionType, int] = {SubscriptionType.standard: 1000}
 
 
 @router.post("/hold/subscribe")
@@ -14,12 +18,19 @@ async def subscribe(body: HoldPostSubscribeBody):
     all_balances = await fetch_hold_balances()
     address = body.account.address.upper()
     balance = all_balances.get(address, None)
+    required_hold_amount = ltai_hold_prices.get(body.type, None)
+
     if balance is None:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail=f"Address {body.account.address} not found in holders list"
         )
 
     # TODO: check if existing subscriptions with hold tier that would reduce available amounts
+    if required_hold_amount is None or balance < required_hold_amount:
+        raise HTTPException(
+            status_code=HTTPStatus.FORBIDDEN,
+            detail=f"Not enough tokens held to pay this subscription (held and available {balance}, required {required_hold_amount})",
+        )
 
     return balance
 
