2e4ad8b806ada20e8038257033dc8f553cd312d2
Reza Rahemtola <reza.rahemtola@epitech.eu>
2025-03-21 00:35:14 +0900

    feat: Started credits system

--- src/config.py
+++ src/config.py
@@ -1,74 +1,16 @@
-import json
 import os
 
 from dotenv import load_dotenv
-from libertai_utils.interfaces.subscription import SubscriptionDefinition, SubscriptionType, SubscriptionProvider
-
-from src.interfaces.subs import SubsConfig, SubsChain
 
 
 class _Config:
-    ALEPH_API_URL: str | None
-    LTAI_BALANCES_AGGREGATE_SENDER: str
-    LTAI_BALANCES_AGGREGATE_KEY: str
-
-    ALEPH_POST_SENDER: str
-    ALEPH_OWNER: str
-    ALEPH_POST_SENDER_SK: bytes
-    ALEPH_POST_CHANNEL: str
-    ALEPH_POST_TYPE: str
-
-    VOUCHERS_PASSWORDS: list[str]
-
-    AGENTS_BACKEND_URL: str
-    AGENTS_BACKEND_PASSWORD: str
-
-    SUBS_PROVIDER_CONFIG: SubsConfig
-
-    subscription_plans: list[list[SubscriptionDefinition]]
+    LTAI_BASE_ADDRESS: str
+    LTAI_PAYMENT_RECEIVER_ADDRESS: str
 
     def __init__(self):
         load_dotenv()
-
-        self.ALEPH_API_URL = os.getenv("ALEPH_API_URL")
-        self.LTAI_BALANCES_AGGREGATE_SENDER = os.getenv("LTAI_BALANCES_AGGREGATE_SENDER")
-        self.LTAI_BALANCES_AGGREGATE_KEY = os.getenv("LTAI_BALANCES_AGGREGATE_KEY")
-        self.ALEPH_POST_SENDER = os.getenv("ALEPH_POST_SENDER")
-        self.ALEPH_OWNER = os.getenv("ALEPH_OWNER")
-        self.ALEPH_POST_SENDER_SK = os.getenv("ALEPH_POST_SENDER_SK")  # type: ignore
-        self.ALEPH_POST_CHANNEL = os.getenv("ALEPH_POST_CHANNEL", "libertai")
-        self.ALEPH_POST_TYPE = os.getenv("ALEPH_POST_TYPE", "libertai-subscription")
-
-        self.VOUCHERS_PASSWORDS = json.loads(os.environ["VOUCHERS_PASSWORDS"])
-
-        self.AGENTS_BACKEND_URL = os.getenv("AGENTS_BACKEND_URL")
-        self.AGENTS_BACKEND_PASSWORD = os.getenv("AGENTS_BACKEND_PASSWORD")
-
-        self.SUBS_PROVIDER_CONFIG = SubsConfig(
-            api_url=os.getenv("SUBS_API_URL", "https://api.subsprotocol.com"),
-            api_key=os.getenv("SUBS_API_KEY"),
-            chain=os.getenv("SUBS_CHAIN", SubsChain.bsc),
-            chain_rpc=os.getenv("SUBS_CHAIN_RPC", "https://binance.llamarpc.com"),
-            app_id=os.getenv("SUBS_APP_ID", 6),
-        )
-
-        self.subscription_plans = [
-            [
-                SubscriptionDefinition(
-                    type=SubscriptionType.pro,
-                    providers=[SubscriptionProvider.hold, SubscriptionProvider.subs, SubscriptionProvider.vouchers],
-                ),
-                SubscriptionDefinition(
-                    type=SubscriptionType.advanced,
-                    providers=[SubscriptionProvider.hold, SubscriptionProvider.subs, SubscriptionProvider.vouchers],
-                ),
-            ],
-            [
-                SubscriptionDefinition(
-                    type=SubscriptionType.agent, providers=[SubscriptionProvider.vouchers], multiple=True
-                )
-            ],
-        ]
+        self.LTAI_BASE_ADDRESS = os.getenv("LTAI_BASE_ADDRESS")
+        self.LTAI_PAYMENT_RECEIVER_ADDRESS = os.getenv("LTAI_PAYMENT_RECEIVER_ADDRESS")
 
 
 config = _Config()
--- src/main.py
+++ src/main.py
@@ -1,61 +1,18 @@
-from http import HTTPStatus
-
 from fastapi import FastAPI
-from fastapi import HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from libertai_utils.chains.index import format_address
-from libertai_utils.interfaces.subscription import (
-    GetUserSubscriptionsResponse,
-    BaseSubscription,
-    SubscriptionChain,
-    Subscription,
-)
 
-from src.providers.hold import router as hold_router
-from src.providers.subs import router as subs_router
-from src.providers.vouchers import router as vouchers_router
+from src.credits import router as credits_router
 from src.utils.cron import lifespan
-from src.utils.subscription import fetch_subscriptions
 
-app = FastAPI(title="LibertAI subscriptions", lifespan=lifespan)
+app = FastAPI(title="LibertAI inference", lifespan=lifespan)
 
-origins = [
-    "https://chat.libertai.io",
-    "http://localhost:9000",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-@app.get("/subscriptions", tags=["General"])
-async def get_user_subscriptions(address: str, chain: SubscriptionChain) -> GetUserSubscriptionsResponse:
-    """Get all the subscriptions of a user"""
-    formatted_address = format_address(address, chain)
-    subscriptions = await fetch_subscriptions(addresses=[formatted_address])
-
-    return GetUserSubscriptionsResponse(subscriptions=[BaseSubscription(**sub.dict()) for sub in subscriptions])
-
-
-@app.get("/subscriptions/{subscription_id}", tags=["General"])
-async def get_subscription(subscription_id: str) -> Subscription:
-    """Get a single subscription data by its ID"""
-    subscriptions = await fetch_subscriptions(subscription_ids=[subscription_id])
-
-    if len(subscriptions) != 1:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND,
-            detail=f"Subscription with ID {subscription_id} not found.",
-        )
-    subscription = subscriptions[0]
-
-    return Subscription(**subscription.dict())
-
-
-app.include_router(hold_router)
-app.include_router(subs_router)
-app.include_router(vouchers_router)
+app.include_router(credits_router)
