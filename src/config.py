657d9d49d275c71e8b883257b588b4b85f2cc104
Reza Rahemtola <reza.rahemtola@epitech.eu>
2025-03-21 18:49:03 +0900

    feat: LTAIPaymentProcessor event fetcher

--- src/config.py
+++ src/config.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 
 class _Config:
     LTAI_BASE_ADDRESS: str
-    LTAI_PAYMENT_RECEIVER_ADDRESS: str
+    LTAI_PAYMENT_PROCESSOR_CONTRACT: str
     DATABASE_URL: str
 
     def __init__(self):
         load_dotenv()
         self.LTAI_BASE_ADDRESS = os.getenv("LTAI_BASE_ADDRESS")
-        self.LTAI_PAYMENT_RECEIVER_ADDRESS = os.getenv("LTAI_PAYMENT_RECEIVER_ADDRESS")
+        self.LTAI_PAYMENT_PROCESSOR_CONTRACT = os.getenv("LTAI_PAYMENT_PROCESSOR_CONTRACT")
         self.DATABASE_URL = os.getenv("DATABASE_URL")
 
 
