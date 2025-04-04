4f5ca695235397e8ecd804822d2fcb504b3ea55b
aliel <ali.elb@gmail.com>
2025-04-04 12:33:09 +0200

    db: increase pool_size

--- src/models/base.py
+++ src/models/base.py
@@ -1,9 +1,7 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy.orm import declarative_base, sessionmaker
 from src.config import config
 
 Base = declarative_base()
-engine = create_engine(config.DATABASE_URL)
+engine = create_engine(config.DATABASE_URL, pool_size=20, max_overflow=0)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
