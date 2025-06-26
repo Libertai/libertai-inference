0a772c11e9f25d910fa782dc2d78f9b87fff632a
Molaryy <molaryyios@gmail.com>
2025-06-26 03:30:10 +0200

    feat: LTAI solana cli

---
+++ contracts/solana/cli/commands/removeAdmin.ts
@@ -0,0 +1,56 @@
+import { Connection, Keypair, PublicKey, sendAndConfirmTransaction, Transaction } from "@solana/web3.js";
+import { program } from "..";
+import { getKeypair } from "../utils";
+import { Program, BN } from "@coral-xyz/anchor";
+import { LibertAiPaymentProcessor } from "../../target/types/libert_ai_payment_processor";
+import idl from "../../target/idl/libert_ai_payment_processor.json";
+
+const removeAdmin = async (
+  payer: Keypair,
+  admin: PublicKey,
+  program: Program,
+) => {
+  const ix = await program.methods
+    .removeAdmin(admin)
+    .accounts({
+      payer: payer.publicKey,
+    })
+    .instruction();
+
+  const tx = new Transaction().add(ix);
+  const sig = await sendAndConfirmTransaction(program.provider.connection, tx, [payer]);
+  console.log(`✅ Removed admin ${admin.toString()}. Tx Signature: ${sig}`);
+}
+
+export const RemoveAdminCommand = async () => {
+  const opts = program.opts();
+  
+  const payer = getKeypair({
+    filepath: opts.payerKeyFilepath,
+    key: opts.payerPrivateKey,
+  });
+  const connection = new Connection(opts.jsonRpcEndpoint, "confirmed");
+  const wallet = {
+    publicKey: payer.publicKey,
+    signTransaction: async (tx: Transaction) => {
+      tx.partialSign(payer);
+      return tx;
+    },
+    signAllTransactions: async (txs: Transaction[]) => {
+      txs.forEach(tx => tx.partialSign(payer));
+      return txs;
+    },
+  };
+  const anchorProgram = new Program(idl as LibertAiPaymentProcessor, {
+    connection,
+    publicKey: wallet.publicKey,
+  });
+
+  const admin = new PublicKey(opts.admin)
+
+  await removeAdmin(
+    payer,
+    admin,
+    anchorProgram
+  );
+}
