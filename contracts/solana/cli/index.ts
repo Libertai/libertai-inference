be3e992ab5f4fe2ed127f485c7953a768c6811e9
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2025-07-29 11:05:41 +0200

    feat(cli): added default payer key filepath instead of always providi… (#20)

--- contracts/solana/cli/index.ts
+++ contracts/solana/cli/index.ts
@@ -8,17 +8,19 @@ import { ChangeOwnerCommand } from "./commands/changeOwner";
 import { GetAdminsCommand } from "./commands/getAdmins";
 import { WithdrawCommand } from "./commands/withdraw";
 import { WithdrawSolCommand } from "./commands/withdraw_sol";
+import * as os from "os";
 
 export const program = new Command();
+const homeDir = os.homedir();
 
 program
-  .name("libert")
-  .description("Libert AI Payment Processor CLI")
-  .option("--payer-key-filepath <path>", "Path to payer private key JSON file")
+  .name("libertai")
+  .description("LibertAI Payment Processor CLI")
+  .option("--payer-key-filepath <path>", "Path to payer private key JSON file", `${homeDir}/.config/solana/id.json`)
   .option("--payer-private-key <key>", "Payer private key as JSON string")
   .option("--json-rpc-endpoint <url>", "Solana RPC endpoint", "https://api.devnet.solana.com")
   .option("--amount <amount>", "Amount of tokens to transfer (in human-readable format, e.g. 60 for 60 tokens)")
-  .option("--token-mint <address>", "Token mint address", "3onmcmVmxyuhKyprEw4LyfdpqTPW6fRA7JQhopbiph5k")
+  .option("--token-mint <address>", "Token mint address", "43czCi4Pd6JKhuKBbKQdZXkZN8JRaWYnRyjj8zWqGF1Z")
   .option("--admin <address>", "Admin address")
   .option("--new-owner <address>", "New owner address")
   .option("--destination <address>", "Destination wallet address for withdrawal")
