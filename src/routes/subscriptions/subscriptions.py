1fac2d9677be1b51614364786474a7d1e0a0f4ca
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2025-08-14 15:57:49 +0200

    feat: added subscriptions routes (#24)
    * feat: added subscriptions routes
    
    * fix: Add types
    
    ---------
    
    Co-authored-by: Reza Rahemtola <reza.rahemtola@epitech.eu>

--- src/routes/subscriptions/subscriptions.py
+++ src/routes/subscriptions/subscriptions.py
@@ -4,7 +4,8 @@ from http import HTTPStatus
 
 from fastapi import Depends, HTTPException
 
-from src.models import SubscriptionStatus, Subscription
+from src.interfaces.subscription import SubscriptionTransactionResponse, SubscriptionResponse
+from src.models import SubscriptionStatus, Subscription, SubscriptionTransaction
 from src.models.base import SessionLocal
 from src.routes.subscriptions import router
 from src.services.auth import get_current_address
@@ -13,6 +14,45 @@ from src.utils.cron import scheduler
 from src.utils.logger import setup_logger
 
 logger = setup_logger(__name__)
+
+
+@router.get("/{subscription_id}/transactions", description="Get subscription transactions")  # type: ignore
+async def get_subscription_transactions(
+    subscription_id: str, user_address: str = Depends(get_current_address)
+) -> list[SubscriptionTransactionResponse]:
+    with SessionLocal() as db:
+        subscription = db.query(Subscription).filter(Subscription.id == subscription_id).first()
+        if not subscription:
+            raise HTTPException(
+                status_code=HTTPStatus.NOT_FOUND,
+                detail=f"Subscription with ID {subscription_id} not found.",
+            )
+        if subscription.user_address != user_address:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail=f"The subscription {subscription_id} does not belong to the user {user_address}.",
+            )
+        subscription_transactions = (
+            db.query(SubscriptionTransaction).filter(SubscriptionTransaction.subscription_id == subscription_id).all()
+        )
+        return [SubscriptionTransactionResponse(**tx.__dict__) for tx in subscription_transactions]
+
+
+@router.get("", description="Get subscriptions for the logged in user")  # type: ignore
+async def get_subscription_by_user_address(
+    user_address: str = Depends(get_current_address),
+) -> list[SubscriptionResponse]:
+    with SessionLocal() as db:
+        subscriptions: list[Subscription] = (
+            db.query(Subscription).filter(Subscription.user_address == user_address).all()
+        )
+
+        if not subscriptions:
+            raise HTTPException(
+                status_code=HTTPStatus.NOT_FOUND,
+                detail=f"User {user_address} doesn't have subscriptions.",
+            )
+        return [SubscriptionResponse(**sub.__dict__) for sub in subscriptions]
 
 
 @router.delete("/{subscription_id}", description="Cancel a subscription")  # type: ignore
