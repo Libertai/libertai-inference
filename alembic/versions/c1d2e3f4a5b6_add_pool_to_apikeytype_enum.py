444a5e77f4599c1ac4adc3c6fea0ee89c6801423
Reza Rahemtola <reza.rahemtola@epitech.eu>
2026-06-09 13:38:39 +0200

    feat(api-keys): migration adding 'pool' enum value

---
+++ alembic/versions/c1d2e3f4a5b6_add_pool_to_apikeytype_enum.py
@@ -0,0 +1,29 @@
+"""add pool to apikeytype enum
+
+Revision ID: c1d2e3f4a5b6
+Revises: c3d4e5f6a7b8
+Create Date: 2026-06-09 00:00:00.000000
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision: str = "c1d2e3f4a5b6"
+down_revision: Union[str, None] = "c3d4e5f6a7b8"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.execute("ALTER TYPE apikeytype ADD VALUE IF NOT EXISTS 'pool'")
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    # PostgreSQL doesn't support removing values from enums
+    pass
